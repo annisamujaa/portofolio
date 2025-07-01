543a69472b23e4c1ddd0fe44f24ea476ef6f93e4
annisamujaa <annisama2611@gmail.com>
2025-07-01 16:07:45 +0700

    first commit

---
+++ assets/js/style.js
@@ -0,0 +1,11 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   var typed = new Typed("#typed-text", {
+//     strings: ["Welcome to my personal website 👋"],
+//     typeSpeed: 100,
+//     backSpeed: 50,
+//     // showCursor: true,
+//     loop: true,
+//     // cursorChar: '|',
+//   });
+// });
+
